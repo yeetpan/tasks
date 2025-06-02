aef470e3ff7e5c541eb053a6063d5508305a3353
Swayam Tiwari <tiwari.swayam@yahoo.com>
2025-06-02 08:45:00 +0530

    minor changes in ExceptionAssgmt

--- d4/src/ExceptionAssgmt.java
+++ d4/src/ExceptionAssgmt.java
@@ -1,44 +1,57 @@
 import java.util.Scanner;
 
-class CustomException extends Exception{
-    public CustomException(String message){
+class CustomException extends Exception {
+    public CustomException(String message) {
         super(message);
     }
 }
+
 public class ExceptionAssgmt {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        try{
-        // Null pointer exception
-            String str=null;
-            System.out.println(str);  // throws NullPointerException
-        //NumberFormatException
+        Scanner sc = new Scanner(System.in);
+
+        // NullPointerException
+        try {
+            String str = null;
+            System.out.println(str.length()); // triggers NullPointerException
+        } catch (NullPointerException e) {
+            System.out.println("Caught a NullPointerException: " + e.getMessage());
+        }
+
+        // NumberFormatException
+        try {
+            System.out.println("Enter a number:");
             String input = sc.nextLine(); // Read input as String
             int num = Integer.parseInt(input); // Try to parse as int
             System.out.println("You entered: " + num);
-        //IndexOutofBound Exception
-            String[] lottery={"1M","250M","5M"};
-            System.out.println("Enter a number to win Lottery");
-            int ltno=sc.nextInt();
-            System.out.println("Congrats");
-        //CustomException
-            int n=sc.nextInt();
-            if(n<0){
-                throw new CustomException("No number below 0");
-            }
-
-        }
-        catch (NullPointerException e) {
-            System.out.println("Caught a NullPointerException: " + e.getMessage());
         } catch (NumberFormatException e) {
             System.out.println("Caught a NumberFormatException: " + e.getMessage());
         }
-        catch(IndexOutOfBoundsException e)
-        {
-            System.out.println("Warning:.....Wrong choice, try again");
-        } catch (CustomException e) {
-            System.out.println(e.getMessage());
+
+        // IndexOutOfBoundsException
+        try {
+            String[] lottery = {"1M", "250M", "5M"};
+            System.out.println("Enter a number between (0-10) to win Lottery:");
+            int ltno = sc.nextInt();
+            String prize = lottery[ltno];
+            System.out.println("Congrats! You won " + prize);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(" Bad Luck .");
         }
 
+        // CustomException
+        try {
+            System.out.println("Enter a number (positive only):");
+            int n = sc.nextInt();
+            if (n < 0) {
+                throw new CustomException("No number below 0 allowed.");
+            } else {
+                System.out.println("Valid input: " + n);
+            }
+        } catch (CustomException e) {
+            System.out.println("Caught CustomException: " + e.getMessage());
+        }
+
+        sc.close();
     }
 }
